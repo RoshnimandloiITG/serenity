cabe9285e9e90e9c7df7f11f633f2ad328a4a71b
Roshni <roshni@itgeeks.com>
2025-10-14 20:30:09 +0530

    other chnages

--- js/script.js
+++ js/script.js
@@ -1,16 +1,21 @@
-  var swiper = new Swiper(".mySwiper", {
-      spaceBetween: 30,
-      centeredSlides: true,
-      autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-      },
+  // var swiper = new Swiper(".mySwiper", {
+  //     spaceBetween: 30,
+  //     centeredSlides: true,
+  //     autoplay: {
+  //       delay: 2500,
+  //       disableOnInteraction: false,
+  //     },
+  //     pagination: {
+  //       el: ".swiper-pagination",
+  //       clickable: true,
+  //     },
+  //     navigation: {
+  //       nextEl: ".swiper-button-next",
+  //       prevEl: ".swiper-button-prev",
+  //     },
+  //   });
+      var swiper = new Swiper(".mySwiper", {
       pagination: {
         el: ".swiper-pagination",
-        clickable: true,
-      },
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
       },
     });
